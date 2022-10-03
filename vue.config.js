56d4047dafb7dbe6a26ad0efdbbb75321fbcf0b7
jDigabri <johndigabriele@gmail.com>
2022-10-03 13:50:16 -0400

    retry

--- vue.config.js
+++ vue.config.js
@@ -1,11 +1,10 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-
   transpileDependencies: [
     'vuetify'
   ]
 
 })
 module.exports = {
-  publicPath: "/Primes_vuejs/"
+  publicPath: process.env.NODE_ENV === "production" ? "/Primes_vuejs/" : "/",
 };
